86938866e52e453fcf8d5aa886bdfb7219848c3d
Emma Cohanim <emmacohanim@gmail.com>
2022-09-03 15:19:24 -0700

    commit tree a244d38bd23d6205f02c40b0f4cf0294dba95326

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
+# monkey patching
+  # adding methods to or altering built-in classes
+  # should not be used in real world bc alteration can result in later bugs for self or collaborators
+
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    # returns true if string ends in period
+   self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    
   end
 end
